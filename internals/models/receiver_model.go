5ec59dec2b3ecb91f91e4be335c926eb1bb4e8bd
Magowtham <magowtham7@gmail.com>
2025-01-18 11:28:29 +0530

    created receiver interface in models

--- internals/models/receiver_model.go
+++ internals/models/receiver_model.go
@@ -1,5 +1,7 @@
 package models
 
+import "net/http"
+
 type Receiver struct {
 	ReceiverId        string `json:"receiver_id"`
 	ReceiverName      string `json:"receiver_name" validate:"required,max=50"`
@@ -8,4 +10,10 @@ type Receiver struct {
 	ReceiverState     string `json:"receiver_state" validate:"required,max=50"`
 	ReceiverStateCode string `json:"receiver_state_code" validate:"required,max=50"`
 	UserId            string `json:"user_id" validate:"required,max=100"`
+}
+
+type ReceiverInterface interface {
+	CreateReceiver(*http.Request) error
+	DeleteReceiver(*http.Request) error
+	GetReceivers(*http.Request) ([]*Receiver, error)
 }
