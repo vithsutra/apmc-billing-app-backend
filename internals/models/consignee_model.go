1162c577d65692c9bf14b4e22db15320b6f87182
Magowtham <magowtham7@gmail.com>
2025-01-29 00:51:34 +0530

    updated consignee model

--- internals/models/consignee_model.go
+++ internals/models/consignee_model.go
@@ -7,7 +7,7 @@ type Consignee struct {
 	ConsigneeName        string `json:"consignee_name" validate:"required,max =50"`
 	ConsigneeAddress     string `json:"consignee_address" validate:"required, max=100"`
 	ConsigneeGstin       string `json:"consignee_gstin" validate:"require,max=50"`
-	ConsigneePhoneNumber string `json:"consignee_phoone_number" validate:"required,max=50"`
+	ConsigneePhoneNumber string `json:"consignee_phone_number" validate:"required,max=50"`
 	ConsigneeState       string `json:"consignee_state" validate:"required,max=50"`
 	ConsigneeStateCode   string `json:"consignee_state_code" validate:"required,max=50"`
 	UserId               string `json:"user_id" validate:"required,max=100"`
