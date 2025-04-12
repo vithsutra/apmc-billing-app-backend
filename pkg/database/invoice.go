44d7e68f758b0619d152885879c5b604f3a18572
Vithsutra Tech <vithsutra.tech@gmail.com>
2025-04-12 18:46:30 +0530

    Update invoice.go
    updated the issues in bug

--- pkg/database/invoice.go
+++ pkg/database/invoice.go
@@ -27,7 +27,7 @@ func (q *Query) CreateInvoice(invoice *models.Invoice) error {
 		billed_id,
 		shipped_id,
 		biller_id,
-		bank__id
+		bank_id
 	) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18)`
 
 	_, err := q.db.Exec(
